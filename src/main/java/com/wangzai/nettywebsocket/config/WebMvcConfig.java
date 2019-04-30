8e4c82af87e86d581a4fef13a09ff91395aa98a0
wbx1442 <wbx123450@163.com>
2019-04-30 22:16:42 +0800

    1.2

--- src/main/java/com/wangzai/nettywebsocket/config/WebMvcConfig.java
+++ src/main/java/com/wangzai/nettywebsocket/config/WebMvcConfig.java
@@ -1,6 +1,6 @@
 package com.wangzai.nettywebsocket.config;
 
-import com.wangzai.nettywebsocket.interceptor.LoginInterceptor;
+import com.wangzai.nettywebsocket.interceptor.RoomInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,7 +11,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
-        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/user/login").excludePathPatterns("/");
+//        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/user/login").excludePathPatterns("/");
+        registry.addInterceptor(new RoomInterceptor()).addPathPatterns("/**").excludePathPatterns("/room/exit");
 
     }
 }
