a20e83cf545c754c094dcbbb3c440c8f69ddd7d3
mafumaru <247306921@qq.com>
2019-06-06 22:15:49 +0800

    nested git

--- src/main/java/com/wangzai/nettywebsocket/config/WebMvcConfig.java
+++ src/main/java/com/wangzai/nettywebsocket/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
